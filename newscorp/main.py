0c9579c2fd0447b3b8cbc6973f76a91a178e4042
Albert Kolozsvari <albertartk@gmail.com>
2023-12-26 21:27:09 +0200

    Update main.py
    Put it all together:
    Stage 1: Read the file
    Stage 2: Process InputRows into OutputRows and get top 3
    Stage 3: Write the file

--- newscorp/main.py
+++ newscorp/main.py
@@ -1,1 +1,19 @@
-print("Newscorp")
+from newscorp.modules.data_modules import HitlogInputRow
+from newscorp.modules.data_processor import (
+    get_top_articles,
+    process_influential_articles,
+)
+from newscorp.modules.file_handler import read_file, write_file
+
+INPUT_FILE_PATH = "data/input/sample_hitlog.csv"
+OUTPUT_FILE_PATH = "data/output/top.csv"
+
+# Read input file
+rows = read_file(INPUT_FILE_PATH, HitlogInputRow)
+
+# Process data
+article_stats = process_influential_articles(rows)
+top_articles = get_top_articles(article_stats, 3)
+
+# Write output file
+write_file(OUTPUT_FILE_PATH, top_articles)
